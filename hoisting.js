a1b407466b59cebb6d6b30ec1e7021b19fd3c8b6
Toni Lamont <tlam212@gmail.com>
2018-05-16 18:50:05 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,30 @@
-// function callMe() {
-//   console.log("I just met you...");
-//   console.log("and this is crazy..");
-//   console.log("but here's my number..");
-//   console.log("so call me");
+function callMe() {
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
  
-//   var lyric = "maybe";
-//   return lyric;
-// }
+  var lyric = "maybe";
+  return lyric;
+}
 
 
-// function crazy() {
-//   // fix the code in here:
+function crazy() {
+  // fix the code in here:
 
-//   var thisIsCrazy = function (){
-//     console.log("hey!!!")
-//   }
-// }thisIsCrazy();
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+}thisIsCrazy();
 
-// function sayMyName() {
-//   // fix the code in here:
-//   var name = "Cricky";
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
 
-//   sayMy();
+  sayMy();
 
-//   function sayMy() {
-//     console.log(name);
-//     var name = "Kristin";
-//   }
-// }
+  function sayMy() {
+    console.log(name);
+    var name = "Kristin";
+  }
+}
