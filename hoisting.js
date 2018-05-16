20e15d6fe488e28115a8a33f8fd7b3c11a6b331f
Toni Lamont <tlam212@gmail.com>
2018-05-16 18:47:52 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,12 @@
-function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
+// function callMe() {
+//   console.log("I just met you...");
+//   console.log("and this is crazy..");
+//   console.log("but here's my number..");
+//   console.log("so call me");
  
-  var lyric = "maybe";
-  return lyric;
-}
+//   var lyric = "maybe";
+//   return lyric;
+// }
 
 
 // function crazy() {
